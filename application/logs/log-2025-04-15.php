c5e27e003941625cc9d49b07d1ffae840ef110de
tech-girish <girish.extravis@gmail.com>
2025-04-15 08:52:08 +0530

    Cas

---
+++ application/logs/log-2025-04-15.php
@@ -0,0 +1,67 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-04-15 05:17:00 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-04-15 05:17:11 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-04-15 05:17:19 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-04-15 05:17:19 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-04-15 05:17:21 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-04-15 05:17:27 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-04-15 05:17:27 --> Severity: Warning --> Undefined property: stdClass::$scrutinity G:\xampp\htdocs\drait_careers\application\controllers\Main.php 172
+ERROR - 2025-04-15 05:17:27 --> Severity: Warning --> Undefined property: stdClass::$application G:\xampp\htdocs\drait_careers\application\controllers\Main.php 184
+ERROR - 2025-04-15 05:17:27 --> Severity: Warning --> Undefined property: stdClass::$post_of G:\xampp\htdocs\drait_careers\application\controllers\Main.php 187
+ERROR - 2025-04-15 05:17:27 --> Severity: Warning --> Undefined property: stdClass::$payment_date G:\xampp\htdocs\drait_careers\application\controllers\Main.php 190
+ERROR - 2025-04-15 05:17:29 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "post_of" on null G:\xampp\htdocs\drait_careers\application\controllers\Main.php 236
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "post_of" on null G:\xampp\htdocs\drait_careers\application\controllers\Main.php 240
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "post_id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 40
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "application" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 76
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "profile_pic" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 87
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "candidate_name" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 95
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "email" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 99
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "post_of" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 105
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "department" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 109
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "mobile" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 121
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "date_of_birth" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 125
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "place_of_birth" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 129
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "religion" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 133
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "father_name" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 139
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "father_occupation" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 143
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "address" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 147
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "reservation_category" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 151
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 442
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "post_id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 455
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "payment_date" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 475
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "txn_id" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 476
+ERROR - 2025-04-15 05:17:29 --> Severity: Warning --> Attempt to read property "payment_date" on null G:\xampp\htdocs\drait_careers\application\views\recruitment\view_print.php 487
+ERROR - 2025-04-15 05:19:08 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-04-15 08:49:08 --> Severity: Warning --> Undefined array key "candidate_name" G:\xampp\htdocs\drait_careers\application\views\template\header.php 560
+ERROR - 2025-04-15 05:21:21 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-04-15 08:51:21 --> Severity: Warning --> Undefined array key "candidate_name" G:\xampp\htdocs\drait_careers\application\views\template\header.php 560
+ERROR - 2025-04-15 05:21:44 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-04-15 08:51:45 --> Severity: Warning --> Undefined array key "candidate_name" G:\xampp\htdocs\drait_careers\application\views\template\header.php 560
--- application/views/home.php
+++ application/views/home.php
@@ -1,90 +1,87 @@
+<!-- Career Openings Section -->
+<section class="career-current-opening">
+    <div class="container">
+        <h2 class="">Current Openings</h2>
+        <div class="row">
 
+            <?php
 
-    <!-- Career Openings Section -->
-    <section class="career-current-opening">
-        <div class="container">
-            <h2 class="">Current Openings</h2>
-            <div class="row">
-
-			<?php 
-            
-            if($recruitmentList) {
-            foreach ($recruitmentList as $recruitmentList1) { ?>
-                <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
-                    <div class="carer_wrappper">
-                        <div class="career-opening">
-                            <h3><a href="<?= base_url('recruitment');?>"><?= $recruitmentList1->title ;?></a></h3>
-                            <div class="years-current"><i class="fa fa-briefcase" aria-hidden="true"></i>   <?= $recruitmentList1->department_names; ?></div>
-                            <div class="place-current"><i class="fa fa-location-arrow" aria-hidden="true"></i> Bengaluru</div>
-                        </div>
-                        <div class="career-apply">
-                            <div class="apply-now"><a target="_blank" href="<?= base_url('recruitment');?>">Apply Now</a></div>
-                            <div class="read-more"><a href="<?= base_url('career-detail');?>/<?= $recruitmentList1->slug ;?>"><i class="fa fa-angle-right" aria-hidden="true"></i></a></div>
+            if ($recruitmentList) {
+                foreach ($recruitmentList as $recruitmentList1) { ?>
+                    <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+                        <div class="carer_wrappper">
+                            <div class="career-opening">
+                                <h3><a href="<?= base_url('recruitment'); ?>"><?= $recruitmentList1->title; ?></a></h3>
+                                <div class="years-current"><i class="fa fa-briefcase" aria-hidden="true"></i> <?= $recruitmentList1->department_names; ?></div>
+                                <div class="place-current"><i class="fa fa-location-arrow" aria-hidden="true"></i> Bengaluru</div>
+                            </div>
+                            <div class="career-apply">
+                                <div class="apply-now"><a target="_blank" href="<?= base_url('recruitment'); ?>">Apply Now</a></div>
+                                <div class="read-more"><a href="<?= base_url('career-detail'); ?>/<?= $recruitmentList1->slug; ?>"><i class="fa fa-angle-right" aria-hidden="true"></i></a></div>
+                            </div>
                         </div>
                     </div>
-                </div>
-<?php } } else {?>
-    <h3 class="">No Openings</h3>
-    <?php }?>
+                <?php }
+            } else { ?>
+                <h3 class="">No Openings</h3>
+            <?php } ?>
 
-            </div>
-            <h2 class="">INTERNAL - CAREER APPRAISAL SCHEME</h2>
-            <div class="row">
-
-                <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
-                    <div class="carer_wrappper">
-                        <div class="career-opening">
-                            <h3><a href="<?= base_url('');?>/assets/internal/SelfAppraisalDrAIT2024final.docx">CAS<br> Application Form<br></a></h3>
-                            <p>(AGP Rs 6000 to AGP 7000)</p>
-                            
-                        </div>
-                        <div class="career-apply">
-                            <div class="apply-now apply-now-new"><a target="_blank" href="<?= base_url('');?>/assets/internal/SelfAppraisalDrAIT2024final.docx">Download</a></div>
-                            <div class="apply-now apply-now-new"><a target="_blank" href="<?= base_url('');?>/assets/internal/WebsiteCASApr2025.docx">Guidelines</a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
-                    <div class="carer_wrappper">
-                        <div class="career-opening">
-                            <h3><a href="<?= base_url('');?>/assets/internal/SelfAppraisalDrAIT2024final.docx">CAS<br> Application Form<br></a></h3>
-                            <p>(AGP Rs 7000 to AGP 8000)</p>
-                        </div>
-                        <div class="career-apply">
-                            <div class="apply-now apply-now-new"><a target="_blank" href="<?= base_url('');?>/assets/internal/SelfAppraisalDrAIT2024final.docx">Download</a></div>
-                            <div class="apply-now apply-now-new"><a target="_blank" href="<?= base_url('');?>/assets/internal/WebsiteCASApr2025.docx">Guidelines</a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
-                    <div class="carer_wrappper">
-                        <div class="career-opening">
-                            <h3><a href="<?= base_url('');?>/assets/internal/SelfAppraisalDrAIT2024final.docx">CAS <br> Application Form<br></a></h3>
-                            <p>(AGP Rs 8000 to AGP 9000)</p>
-                        </div>
-                        <div class="career-apply">
-                            <div class="apply-now apply-now-new"><a target="_blank" href="<?= base_url('');?>/assets/internal/SelfAppraisalDrAIT2024final.docx">Download</a></div>
-                            <div class="apply-now apply-now-new"><a target="_blank" href="<?= base_url('');?>/assets/internal/WebsiteCASApr2025.docx">Guidelines</a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
-                    <div class="carer_wrappper">
-                        <div class="career-opening">
-                            <h3><a href="<?= base_url('');?>/assets/internal/SelfAppraisalDrAIT2024final.docx">CAS<br> Application Form<br></a></h3>
-                            <p>(AGP Rs 9000 to AGP 10000)</p>
-                        </div>
-                        <div class="career-apply">
-                            <div class="apply-now apply-now-new"><a target="_blank" href="<?= base_url('');?>/assets/internal/SelfAppraisalDrAIT2024final.docx">Download</a></div>
-                            <div class="apply-now apply-now-new"><a target="_blank" href="<?= base_url('');?>/assets/internal/WebsiteCASApr2025.docx">Guidelines</a></div>
-                        </div>
-                    </div>
-                </div>
-               
-
-
-            </div>
         </div>
-    </section>
+        <h2 class="">INTERNAL - CAREER APPRAISAL SCHEME</h2>
+        <div class="row">
 
- 
+            <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
+                <div class="carer_wrappper">
+                    <div class="career-opening">
+                        <h3><a href="<?= base_url(''); ?>/assets/internal/SelfAppraisalDrAIT2024final.docx">CAREER ADVANCEMENT SCHEME<br> Application Form<br></a></h3>
+                        <p>(AGP Rs 6000 to AGP 7000)</p>
+
+                    </div>
+                    <div class="career-apply">
+                        <div class="apply-now apply-now-new"><a target="_blank" href="<?= base_url(''); ?>/assets/internal/WebsiteCASApr2025.docx">Guidelines</a></div>
+                        <div class="apply-now apply-now-new"><a target="_blank" href="<?= base_url(''); ?>/assets/internal/SelfAppraisalDrAIT2024final.docx">Download</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
+                <div class="carer_wrappper">
+                    <div class="career-opening">
+                        <h3><a href="<?= base_url(''); ?>/assets/internal/SelfAppraisalDrAIT2024final.docx">CAREER ADVANCEMENT SCHEME<br> Application Form<br></a></h3>
+                        <p>(AGP Rs 7000 to AGP 8000)</p>
+                    </div>
+                    <div class="career-apply">
+                        <div class="apply-now apply-now-new"><a target="_blank" href="<?= base_url(''); ?>/assets/internal/WebsiteCASApr2025.docx">Guidelines</a></div>
+                        <div class="apply-now apply-now-new"><a target="_blank" href="<?= base_url(''); ?>/assets/internal/SelfAppraisalDrAIT2024final.docx">Download</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
+                <div class="carer_wrappper">
+                    <div class="career-opening">
+                        <h3><a href="<?= base_url(''); ?>/assets/internal/SelfAppraisalDrAIT2024final.docx">CAREER ADVANCEMENT SCHEME<br> Application Form<br></a></h3>
+                        <p>(AGP Rs 8000 to AGP 9000)</p>
+                    </div>
+                    <div class="career-apply">
+                        <div class="apply-now apply-now-new"><a target="_blank" href="<?= base_url(''); ?>/assets/internal/WebsiteCASApr2025.docx">Guidelines</a></div>
+                        <div class="apply-now apply-now-new"><a target="_blank" href="<?= base_url(''); ?>/assets/internal/SelfAppraisalDrAIT2024final.docx">Download</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
+                <div class="carer_wrappper">
+                    <div class="career-opening">
+                        <h3><a href="<?= base_url(''); ?>/assets/internal/SelfAppraisalDrAIT2024final.docx">CAREER ADVANCEMENT SCHEME<br> Application Form<br></a></h3>
+                        <p>(AGP Rs 9000 to AGP 10000)</p>
+                    </div>
+                    <div class="career-apply">
+                        <div class="apply-now apply-now-new"><a target="_blank" href="<?= base_url(''); ?>/assets/internal/WebsiteCASApr2025.docx">Guidelines</a></div>
+                        <div class="apply-now apply-now-new"><a target="_blank" href="<?= base_url(''); ?>/assets/internal/SelfAppraisalDrAIT2024final.docx">Download</a></div>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+</section>
